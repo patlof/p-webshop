9d1c82ce5d83e15132383195a629afeba257b9ed
patlof <patrik.lofvander@yahoo.se>
2026-02-20 10:18:25 +0100

    Build and set upp the mobile menu to work with js

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+// Make mobile navigation work
+const navBtnEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+navBtnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
