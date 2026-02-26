8f350405d3a1d0a0759d97eb236a2397ebe280b7
patlof <patrik.lofvander@yahoo.se>
2026-02-26 14:02:56 +0100

    Set up a productcard and a sqllite db with some test-products

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 import express from "express";
+import Database from "better-sqlite3";
 
 const app = express();
 const port = 3000;
+
+//Connect to db
+const db = new Database("data.db");
+
+// Create table
+db.exec(`CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY,
+        category TEXT NOT NULL,
+        name TEXT NOT NULL,
+        description TEXT,
+        image TEXT,
+        price DECIMAL(6, 2) NOT NULL)`);
+
+//Prepare and insert values
+// const insert = db.prepare(
+//   "INSERT INTO products (category, name, description, image, price) VALUES (?,?, ?, ?,?)",
+// );
+// const productOne = insert.run(
+//   "flowers",
+//   "Rose D",
+//   "A romantic gester starts with a rose",
+//   "img/dahlia_one.jpg",
+//   3.99,
+// );
+// const productTwo = insert.run(
+//   "flowers",
+//   "T Dawn",
+//   "When they bloom the spring is already here!",
+//   "img/dahlia_two.jpg",
+//   1.99,
+// );
+// const productThree = insert.run(
+//   "flowers",
+//   "Lilly D",
+//   "The arom is breathtaking!",
+//   "img/dahlia_three.jpg",
+//   0.99,
+// );
+// const productFour = insert.run(
+//   "flowers",
+//   "Da Dahlia",
+//   "A flower that makes your wife happy!",
+//   "img/dahlia_four.jpg",
+//   5.99,
+// );
+
+// db.close();
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/flowers", (req, res) => {
+  res.render("flowers.ejs");
 });
 
 app.listen(port, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
-// Make mobile navigation work
+////////// Make mobile navigation work //////////
 const navBtnEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 navBtnEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+////////// Get date //////////
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
