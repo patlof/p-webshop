db7044eb72d9af371dc0ab6830399bb7c8a288e6
patlof <patrik.lofvander@yahoo.se>
2026-03-04 16:19:12 +0100

    Fix css queries and prepare for tools with pictures

--- index.js
+++ index.js
@@ -16,42 +16,10 @@ db.exec(`CREATE TABLE IF NOT EXISTS products (
         image TEXT,
         price DECIMAL(6, 2) NOT NULL)`);
 
-//Prepare and insert values
-// const insert = db.prepare(
-//   "INSERT INTO products (category, name, description, image, price) VALUES (?,?, ?, ?,?)",
-// );
-// const productOne = insert.run(
-//   "flowers",
-//   "Rose D",
-//   "A romantic gester starts with a rose",
-//   "img/dahlia_one.jpg",
-//   3.99,
-// );
-// const productTwo = insert.run(
-//   "flowers",
-//   "T Dawn",
-//   "When they bloom the spring is already here!",
-//   "img/dahlia_two.jpg",
-//   1.99,
-// );
-// const productThree = insert.run(
-//   "flowers",
-//   "Lilly D",
-//   "The arom is breathtaking!",
-//   "img/dahlia_three.jpg",
-//   0.99,
-// );
-// const productFour = insert.run(
-//   "flowers",
-//   "Da Dahlia",
-//   "A flower that makes your wife happy!",
-//   "img/dahlia_four.jpg",
-//   5.99,
-// );
+
 
 //Read from database
 const products = db.prepare("SELECT * FROM products").all();
-console.log(products);
 
 app.use(express.static("public"));
 
